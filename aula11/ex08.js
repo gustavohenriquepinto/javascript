d3c51b509871497bc3178483cdd4a46269f607c6
gustavohenriquepinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2022-01-12 20:34:52 -0300

    if

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var vel= 60.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if(vel>60){
+    console.log('Passou da velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
