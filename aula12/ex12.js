60361f637683d92745ebe9c1e6653d401d9bce12
gustavohenriquepinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2022-01-23 17:48:21 -0300

    Create ex12.js

---
+++ aula12/ex12.js
@@ -0,0 +1,20 @@
+var idade= 24
+console.log(`Você tem ${idade} anos.`)
+
+/*if (idade<16){
+    console.log('Você não pode votar')
+}else{
+    if (idade>=16 && idade<18){
+        console.log('Voto opcional')
+    }else{
+        console.log('Voto obrigatório')
+    }
+}*/
+
+if(idade<16){
+    console.log('Não pode votar')
+}else if(idade<18 || idade>=65){
+    console.log('Voto opcional')
+}else{
+    console.log('Voto obrigatório')
+}
