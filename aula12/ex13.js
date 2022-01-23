002272daf190c04ad2c5523dd8efd6a5b5c8203f
gustavohenriquepinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2022-01-23 18:02:29 -0300

    Update ex13.js

---
+++ aula12/ex13.js
@@ -0,0 +1,13 @@
+var agora= new Date()
+
+var hora= agora.getHours
+
+console.log(`Agora são ${hora} horas`)
+
+if (hora>=18 || hora<6){
+    console.log('Boa noite!')
+}else if(hora<12){
+    console.log('Bom dia!')
+}else{
+    console.log('Boa tarde')
+}
