299bb8bb9590523fe91d645d8d3feab5332f35b1
gustavohenriquepinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2022-01-26 11:37:01 -0300

    testes

--- aula12ex/ex15/script.js
+++ aula12ex/ex15/script.js
@@ -1,17 +1,21 @@
 var hora= new Date().getHours()
 var min= new Date().getMinutes()
-var sec= new Date().getSeconds()
 var horario= document.getElementById('hor')
 var h= document.getElementById('h')
+var corpo= document.body
 
 if(hora<6){
     horario.innerText= 'Está de madrugada!'
+    corpo.style.backgroud= 'black'
 }else if(hora<12){
     horario.innerText= 'Está de manhã!'
+    corpo.style.backgroud= 'blue'
 }else if(hora<18){
     horario.innerText= 'Está de tarde!'
+    corpo.style.background= 'orange'
 }else{
     horario.innerText= 'Está de noite!'
+    corpo.style.backgroud= 'gray'
 }
 
-h.innerText= `Agora são ${hora}:${min}:${sec}`
+h.innerText= `Agora são ${hora}:${min}.
